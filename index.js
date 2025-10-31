581951def340786e84f010b4d0375687e58eb38c
chaman7787 <aman367787@gmail.com>
2025-10-31 21:51:40 +0530

    ss

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 8085;
+const port = 8005;
 
 // Serve static files from the React dist directory
 app.use(express.static(path.join(__dirname, 'dist')));
